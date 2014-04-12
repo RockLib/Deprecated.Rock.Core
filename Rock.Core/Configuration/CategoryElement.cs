ab28b8b500011c2001618abd47e9c14a500f47eb
Brian Friesen <brianfriesen@quickenloans.com>
2014-04-12 00:45:15 -0400

    Add config classes for web.config or app.config.

---
+++ Rock.Core/Configuration/CategoryElement.cs
@@ -0,0 +1,47 @@
+﻿using System.Configuration;
+using System.Configuration.Provider;
+
+namespace Rock.Configuration
+{
+    public class CategoryElement : ConfigurationElement
+    {
+        [ConfigurationProperty("name", IsRequired = true, IsKey = true)]
+        public string Name
+        {
+            get
+            {
+                return (string)this["name"];
+            }
+            set
+            {
+                this["name"] = value;
+            }
+        }
+
+        [ConfigurationProperty("providers", IsDefaultCollection = true)]
+        public ProviderCollection Providers
+        {
+            get
+            {
+                return (ProviderCollection)this["providers"]; // questionable
+            }
+            set
+            {
+                this["providers"] = value;
+            }
+        }
+
+        [ConfigurationProperty("throttlingRule")]
+        public string ThrottlingRule
+        {
+            get
+            {
+                return (string)this["throttlingRule"];
+            }
+            set
+            {
+                this["throttlingRule"] = value;
+            }
+        }
+    }
+}
---
+++ Rock.Core/Configuration/RockFrameworkSection.cs
@@ -0,0 +1,20 @@
+﻿using System.Configuration;
+
+namespace Rock.Configuration
+{
+    public class RockFrameworkSection : ConfigurationSection
+    {
+        [ConfigurationProperty("logger", IsRequired = false)]
+        public LoggerSectionGroup LoggerSettings
+        {
+            get
+            {
+                return (LoggerSectionGroup)this["logger"];
+            }
+            set
+            {
+                this["logger"] = value;
+            }
+        }
+    }
+}
