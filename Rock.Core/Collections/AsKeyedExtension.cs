31f3933efc82758c9253b6b466d32e486db5b977
Brian Friesen <brianfriesen@quickenloans.com>
2014-04-12 00:44:03 -0400

    Add AsKeyed extension method.

---
+++ Rock.Core/Collections/AsKeyedExtension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Rock.Collections
+{
+    public static class AsKeyedExtension
+    {
+        public static IKeyedEnumerable<TKey, TItem> AsKeyed<TKey, TItem>(
+            this IEnumerable<TItem> items,
+            Func<TItem, TKey> getKey)
+        {
+            return new FunctionalKeyedCollection<TKey, TItem>(getKey, items);
+        }
+    }
+}
---
+++ Rock.Core/Collections/FunctionalKeyedCollection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Rock.Collections
+{
+    public class FunctionalKeyedCollection<TKey, TItem> : KeyedCollection<TKey, TItem>
+    {
+        private readonly Func<TItem, TKey> _getKey;
+
+        public FunctionalKeyedCollection(Func<TItem, TKey> getKey, IEnumerable<TItem> items = null)
+        {
+            _getKey = getKey;
+
+            if (items != null)
+            {
+                foreach (var item in items)
+                {
+                    Add(item);
+                }
+            }
+        }
+
+        protected override TKey GetKeyForItem(TItem item)
+        {
+            return _getKey(item);
+        }
+    }
+}
