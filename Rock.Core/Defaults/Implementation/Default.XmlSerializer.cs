fd3519282c6260551347434c77aade19c8982d88
Brian Friesen <brianfriesen@quickenloans.com>
2014-05-13 13:55:32 -0400

    Add default xml serializer.

---
+++ Rock.Core/Defaults/Implementation/Default.XmlSerializer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Rock.Serialization;
+
+namespace Rock.Defaults.Implementation
+{
+    public static partial class Default
+    {
+        private static readonly DefaultHelper<ISerializer> _xmlSerializer = new DefaultHelper<ISerializer>(() => new XSerializerSerializer());
+
+        public static ISerializer XmlSerializer
+        {
+            get { return _xmlSerializer.Current; }
+        }
+
+        public static void SetApplicationInfo(Func<ISerializer> getXmlSerializerInstance)
+        {
+            _xmlSerializer.SetCurrent(getXmlSerializerInstance);
+        }
+    }
+}
---
+++ Rock.Core/Serialization/XSerializerSerializer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using XSerializer;
+
+namespace Rock.Serialization
+{
+    public class XSerializerSerializer : ISerializer
+    {
+        public void Serialize(Stream stream, object item, Type type)
+        {
+            var serializer = XmlSerializer.Create(type);
+            serializer.Serialize(stream, item);
+        }
+
+        public object Deserialize(Stream stream, Type type)
+        {
+            var serializer = XmlSerializer.Create(type);
+            return serializer.Deserialize(stream);
+        }
+    }
+}
