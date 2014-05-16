a0e97dd367d7e0e936071ff21e050a54534b1433
Brian Friesen <brianfriesen@quickenloans.com>
2014-05-16 15:46:09 -0400

    Add tests for GetEnumValue extension method.

---
+++ Rock.Core.UnitTests/Extensions/GetEnumValueExtensionTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using Rock.Extensions;
+
+namespace Rock.Core.UnitTests.Extensions
+{
+    public class TheGetEnumValueExtensionMethod
+    {
+        [TestCase("Bar", Foo.Bar, TestName="Case Match")]
+        [TestCase("bar", Foo.Bar, TestName="Case Mismatch")]
+        public void ReturnsAnEnumValueThatCorrespondsToTheGivenStringValue(string stringValue, Foo expectedValue)
+        {
+            var value = stringValue.GetEnumValue<Foo>();
+
+            Assert.That(value, Is.EqualTo(Foo.Bar));
+        }
+
+        [Test]
+        public void ThrowsAnExceptionIfNoEnumValueCorrespondsToTheGivenStringValue()
+        {
+            const string value = "wtf";
+
+            Assert.That(() => value.GetEnumValue<Foo>(), Throws.Exception);
+        }
+
+        [Test]
+        public void ThrowsAnExceptionIfTheGenericTypeIsNotAnEnum()
+        {
+            const string value = "Baz";
+
+            Assert.That(() => value.GetEnumValue<Bar>(), Throws.Exception);
+        }
+
+        public enum Foo
+        {
+            Bar,
+            Baz
+        }
+
+        public struct Bar
+        {
+            public int Baz;
+        }
+    }
+}
