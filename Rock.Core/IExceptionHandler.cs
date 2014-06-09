9ab403ef7a490b65a53b9620c5e2531b25fad5e9
Brian Friesen <brianfriesen@quickenloans.com>
2014-06-09 10:55:16 -0400

    Add exception handler interface and null implementation.

---
+++ Rock.Core/IExceptionHandler.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Rock
+{
+    public interface IExceptionHandler
+    {
+        void HandleException(Exception ex);
+    }
+}
---
+++ Rock.Core/NullExceptionHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Rock
+{
+    public sealed class NullExceptionHandler : IExceptionHandler
+    {
+        private static readonly NullExceptionHandler _instance = new NullExceptionHandler();
+
+        private NullExceptionHandler()
+        {
+        }
+
+        public static NullExceptionHandler Instance
+        {
+            get { return _instance; }
+        }
+
+        public void HandleException(Exception ex)
+        {
+        }
+    }
+}
