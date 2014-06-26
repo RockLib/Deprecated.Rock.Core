d521390d8c27d761fb5d7cbfbc121c81f61e12c6
Brian Friesen <brianfriesen@quickenloans.com>
2014-06-26 10:53:30 -0400

    Add tests for NullExceptionHandler.

---
+++ Rock.Core.UnitTests/NullExceptionHandlerTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NUnit.Framework;
+using Rock;
+
+namespace NullExceptionHandlerTests
+{
+    public class TheNullExceptionHandlerClass
+    {
+        [Test]
+        public void HasNoPublicConstructors()
+        {
+            var publicConstructors = typeof(NullExceptionHandler).GetConstructors();
+
+            Assert.That(publicConstructors, Is.Empty);
+        }
+
+        [Test]
+        public void IsASingleton()
+        {
+            var first = NullExceptionHandler.Instance;
+            var second = NullExceptionHandler.Instance;
+
+            Assert.That(first, Is.SameAs(second));
+        }
+    }
+
+    public class TheHandleExceptionMethod
+    {
+        [Test]
+        public void DoesNothing()
+        {
+            Assert.That(() => NullExceptionHandler.Instance.HandleException(new Exception()), Throws.Nothing);
+        }
+    }
+}
