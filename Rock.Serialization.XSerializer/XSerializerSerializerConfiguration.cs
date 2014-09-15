cc2f14fd633e3d35b99d078ef964e4344a33ea00
Brian Friesen <brianfriesen@quickenloans.com>
2014-09-14 21:28:24 -0400

    Add XSerializerSerializer.

---
+++ Rock.Serialization.XSerializer/XSerializerSerializerConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using System.Xml.Serialization;
+
+namespace Rock.Serialization
+{
+    public class XSerializerSerializerConfiguration : IXSerializerSerializerConfiguration
+    {
+        public XSerializerSerializerConfiguration()
+        {
+            Namespaces = new XmlSerializerNamespaces();
+            Encoding = Encoding.UTF8;
+            Redact = true;
+        }
+
+        public XmlSerializerNamespaces Namespaces { get; set; }
+        public Encoding Encoding { get; set; }
+        public string DefaultNamespace { get; set; }
+        public bool Indent { get; set; }
+        public bool AlwaysEmitTypes { get; set; }
+        public bool Redact { get; set; }
+        public bool TreatEmptyElementAsString { get; set; }
+        public bool EmitNil { get; set; }
+    }
+}
