05539a50260acc4b20c7a5a9c433547d688e30ec
Brian Friesen <brianfriesen@quickenloans.com>
2014-10-17 00:16:13 -0400

    Add XSerializerSectionHandler.

---
+++ Rock.Core.XSerializer/Configuration/XSerializerSectionHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Xml;
+using XSerializer;
+
+namespace Rock.Configuration
+{
+    public class XSerializerSectionHandler : XmlSerializerSectionHandler
+    {
+        protected override object Deserialize(XmlNode section, Type configType)
+        {
+            var serializer = XmlSerializer.Create(configType, o => o.SetRootElementName(section.Name));
+            return serializer.Deserialize(section.OuterXml);
+        }
+    }
+
+    public class XSerializerSectionHandler<TConfiguration> : XSerializerSectionHandler
+    {
+        protected override Type GetConfigType(XmlNode section)
+        {
+            return typeof(TConfiguration);
+        }
+    }
+}
