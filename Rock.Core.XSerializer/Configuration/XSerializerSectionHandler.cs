f517e70a08ed1123c3ae2469008246e97a5b911f
Brian Friesen <brianfriesen@quickenloans.com>
2014-10-17 14:02:25 -0400

    Add xml documentation to XmlSerializerSectionHandler & friends.

--- Rock.Core.XSerializer/Configuration/XSerializerSectionHandler.cs
+++ Rock.Core.XSerializer/Configuration/XSerializerSectionHandler.cs
@@ -4,8 +4,21 @@ using XSerializer;
 
 namespace Rock.Configuration
 {
+    /// <summary>
+    /// A section handler that creates an instance of a type based
+    /// on an attribute named "type" defined in a section element 
+    /// from a *.config file, using the XSerializer serialization
+    /// library to deserialize the element.
+    /// </summary>
     public class XSerializerSectionHandler : XmlSerializerSectionHandler
     {
+        /// <summary>
+        /// Deserialize the configuration object that will be returned by the
+        /// <see cref="XmlSerializerSectionHandler.Create"/> method.
+        /// </summary>
+        /// <param name="section">An xml node that represents configuration object in the *.config file.</param>
+        /// <param name="configType">The type of the object to be returned by the <see cref="Create"/> method.</param>
+        /// <returns>The deserialized object.</returns>
         protected override object Deserialize(XmlNode section, Type configType)
         {
             var serializer = XmlSerializer.Create(configType, o => o.SetRootElementName(section.Name));
@@ -13,8 +26,19 @@ namespace Rock.Configuration
         }
     }
 
+    /// <summary>
+    /// A section handler that creates an instance of the 
+    /// <typeparamref name="TConfiguration"/> type, using the
+    /// XSerializer serialization library to deserialize the element.
+    /// </summary>
     public class XSerializerSectionHandler<TConfiguration> : XSerializerSectionHandler
     {
+        /// <summary>
+        /// Get the type of the configuration object that will be returned by the
+        /// <see cref="XmlSerializerSectionHandler.Create"/> method.
+        /// </summary>
+        /// <param name="section">An xml node that represents configuration object in the *.config file.</param>
+        /// <returns>The type of the configuration object that will be returned by the <see cref="XmlSerializerSectionHandler.Create"/> method.</returns>
         protected override Type GetConfigType(XmlNode section)
         {
             return typeof(TConfiguration);
--- Rock.Core/Configuration/XmlSerializerSectionHandler.cs
+++ Rock.Core/Configuration/XmlSerializerSectionHandler.cs
@@ -6,6 +6,12 @@ using System.Xml.Serialization;
 
 namespace Rock.Configuration
 {
+    /// <summary>
+    /// A section handler that creates an instance of a type based
+    /// on an attribute named "type" defined in a section element 
+    /// from a *.config file, using the an instance of
+    /// <see cref="XmlSerializer"/> to deserialize the element.
+    /// </summary>
     public class XmlSerializerSectionHandler : IConfigurationSectionHandler
     {
         public object Create(object parent, object configContext, XmlNode section)
@@ -13,6 +19,13 @@ namespace Rock.Configuration
             return Deserialize(section, GetConfigType(section));
         }
 
+        /// <summary>
+        /// Deserialize the configuration object that will be returned by the
+        /// <see cref="Create"/> method.
+        /// </summary>
+        /// <param name="section">An xml node that represents configuration object in the *.config file.</param>
+        /// <param name="configType">The type of the object to be returned by the <see cref="Create"/> method.</param>
+        /// <returns>The deserialized object.</returns>
         protected virtual object Deserialize(XmlNode section, Type configType)
         {
             var serializer = new XmlSerializer(configType, new XmlRootAttribute(section.Name));
@@ -23,6 +36,12 @@ namespace Rock.Configuration
             }
         }
 
+        /// <summary>
+        /// Get the type of the configuration object that will be returned by the
+        /// <see cref="Create"/> method.
+        /// </summary>
+        /// <param name="section">An xml node that represents configuration object in the *.config file.</param>
+        /// <returns>The type of the configuration object that will be returned by the <see cref="Create"/> method.</returns>
         protected virtual Type GetConfigType(XmlNode section)
         {
             var xPathNavigator = section.CreateNavigator();
@@ -44,8 +63,19 @@ namespace Rock.Configuration
         }
     }
 
+    /// <summary>
+    /// A section handler that creates an instance of the 
+    /// <typeparamref name="TConfiguration"/> type, using the an instance of
+    /// <see cref="XmlSerializer"/> to deserialize the element.
+    /// </summary>
     public class XmlSerializerSectionHandler<TConfiguration> : XmlSerializerSectionHandler
     {
+        /// <summary>
+        /// Get the type of the configuration object that will be returned by the
+        /// <see cref="XmlSerializerSectionHandler.Create"/> method.
+        /// </summary>
+        /// <param name="section">An xml node that represents configuration object in the *.config file.</param>
+        /// <returns>The type of the configuration object that will be returned by the <see cref="XmlSerializerSectionHandler.Create"/> method.</returns>
         protected override Type GetConfigType(XmlNode section)
         {
             return typeof(TConfiguration);
