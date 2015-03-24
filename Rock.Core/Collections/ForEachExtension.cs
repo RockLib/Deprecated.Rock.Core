ad6c03fd87592200e759fb01bd482a5c20fc6d27
Brian Friesen <brianfriesen@quickenloans.com>
2015-03-24 17:59:39 -0400

    Add ForEach extension method.

---
+++ Rock.Core/Collections/ForEachExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Rock.Collections
+{
+    /// <summary>
+    /// Provides a <see cref="ForEach{TSource}"/> extension method.
+    /// </summary>
+    public static class ForEachExtension
+    {
+        /// <summary>
+        /// Performs the specified action on each element of the <see cref="IEnumerable{T}"/>.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of source.</typeparam>
+        /// <param name="source">
+        /// An <see cref="IEnumerable{T}"/> that contains the elements to perform an action on.
+        /// </param>
+        /// <param name="action">
+        /// The <see cref="Action{T}"/> delegate to perform on each element of the <see cref="IEnumerable{T}"/>.
+        ///</param>
+        public static void ForEach<TSource>(this IEnumerable<TSource> source, Action<TSource> action)
+        {
+            foreach (var item in source)
+            {
+                action(item);
+            }
+        }
+    }
+}
