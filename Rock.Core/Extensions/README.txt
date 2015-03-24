eab6e0706419132402e4784e4c692c45b255f491
Brian Friesen <brianfriesen@quickenloans.com>
2015-03-24 17:19:22 -0400

    Add Truncate extension method for DateTime.

---
+++ Rock.Core/Extensions/README.txt
@@ -0,0 +1,3 @@
+﻿The namespaces of all the types defined in this folder should be "Rock".
+This folder exists purely for organizational purposes.
+If your extension method isn't applicable at the root level namespace, put it somewhere.
---
+++ Rock.Core/Extensions/TimeSection.cs
@@ -0,0 +1,25 @@
+namespace Rock.Extensions
+{
+    /// <summary>
+    /// Defines time sections that can be truncated from a DateTime.
+    /// </summary>
+    public enum TimeSection
+    {
+        /// <summary>
+        /// Indicates that the millisecond component of the DateTime will be truncated.
+        /// </summary>
+        Millisecond,
+        /// <summary>
+        /// Indicates that the second component of the DateTime will be truncated.
+        /// </summary>
+        Second,
+        /// <summary>
+        /// Indicates that the minute component of the DateTime will be truncated.
+        /// </summary>
+        Minute,
+        /// <summary>
+        /// Indicates that the hour component of the DateTime will be truncated.
+        /// </summary>
+        Hour
+    }
+}
