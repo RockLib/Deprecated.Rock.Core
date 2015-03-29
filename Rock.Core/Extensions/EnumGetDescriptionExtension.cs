e02e8afbbb5131f0c7c1da527ccbb7985f35b42c
Brian Friesen <brianfriesen@quickenloans.com>
2015-03-29 05:01:59 -0400

    Add GetDescription extension for the Enum type.

---
+++ Rock.Core/Extensions/EnumGetDescriptionExtension.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace Rock
+{
+    public static class EnumGetDescriptionExtension
+    {
+        private static readonly ConcurrentDictionary<Enum, string> _descriptionCache = new ConcurrentDictionary<Enum, string>();
+
+        /// <summary>
+        /// Gets the description of the enumeration value.
+        /// </summary>
+        /// <param name="enumValue">The enumeration value.</param>
+        /// <returns>The description of the enumeration value, if provided. Else, the result of calling <see cref="object.ToString"/> on the value.</returns>
+        public static string GetDescription(this Enum enumValue)
+        {
+            return
+                _descriptionCache.GetOrAdd(
+                    enumValue,
+                    value =>
+                    {
+                        var field = enumValue.GetType().GetField(enumValue.ToString());
+
+                        if (field != null)
+                        {
+                            var attribute = (DescriptionAttribute)Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute));
+
+                            if (attribute != null)
+                            {
+                                return attribute.Description;
+                            }
+                        }
+
+                        return enumValue.ToString();
+                    });
+        }
+    }
+}
