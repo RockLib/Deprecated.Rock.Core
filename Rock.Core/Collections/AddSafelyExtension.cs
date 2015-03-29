2968abaa0a7f9037e6de47e2a2d310b9779a200c
Brian Friesen <brianfriesen@quickenloans.com>
2015-03-29 16:09:43 -0400

    Add AddSafely extension.

---
+++ Rock.Core/Collections/AddSafelyExtension.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace Rock.Collections
+{
+    /// <summary>
+    /// Provides a <see cref="AddSafely{TKey,TValue}"/> extension method.
+    /// </summary>
+    public static class AddSafelyExtension
+    {
+        /// <summary>
+        /// Adds the specified key and value to the dictionary if and only if the
+        /// key is not currently present.
+        /// </summary>
+        /// <typeparam name="TKey">The type of the key.</typeparam>
+        /// <typeparam name="TValue">The type of the value.</typeparam>
+        /// <param name="source">The source <see cref="Dictionary{TKey,TValue}"/>.</param>
+        /// <param name="key">The key of the element to add.</param>
+        /// <param name="value">The value of the element to add. The value can be null for reference types.</param>
+        public static void AddSafely<TKey, TValue>(this Dictionary<TKey, TValue> source, TKey key, TValue value)
+        {
+            if (!source.ContainsKey(key))
+            {
+                source.Add(key, value);
+            }
+        }
+    }
+}
