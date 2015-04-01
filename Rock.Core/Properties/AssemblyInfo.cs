9e07ce2d702ac587b859bebb55b2c15771b1f80e
Brian Friesen <brianfriesen@quickenloans.com>
2015-04-01 10:11:53 -0400

    Publish nuget packages.

--- Rock.Core/Properties/AssemblyInfo.cs
+++ Rock.Core/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.9.0.0")]
-[assembly: AssemblyFileVersion("0.0.4")]
-[assembly: AssemblyInformationalVersion("0.0.4-alpha13")]
+[assembly: AssemblyFileVersion("0.9.0")]
+[assembly: AssemblyInformationalVersion("0.9.0")]
 
 [assembly: InternalsVisibleTo("Rock.Core.UnitTests")]
