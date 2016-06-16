8f88eeadcc8fb56878f09ff84ecb31f01eb3ca3d
Brian Friesen <brianfriesen@quickenloans.com>
2016-06-16 18:30:33 -0400

    Add CompositeLibraryLogger

---
+++ Rock.Core/LibraryLogging/CompositeLibraryLogger.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Rock.LibraryLogging
+{
+    public class CompositeLibraryLogger : ILibraryLogger
+    {
+        private readonly ILibraryLogger[] _loggers;
+
+        public CompositeLibraryLogger(IEnumerable<ILibraryLogger> loggers)
+            : this(loggers.ToArray())
+        {
+        }
+
+        public CompositeLibraryLogger(params ILibraryLogger[] loggers)
+        {
+            _loggers = loggers;
+        }
+
+        public void Log(LibraryLogMessage message)
+        {
+            foreach (var logger in _loggers)
+            {
+                try
+                {
+                    logger.Log(message);
+                } // ReSharper disable once EmptyGeneralCatchClause
+                catch
+                {
+                }
+            }
+        }
+
+        public void Debug(LibraryLogMessage message)
+        {
+            foreach (var logger in _loggers)
+            {
+                try
+                {
+                    logger.Debug(message);
+                } // ReSharper disable once EmptyGeneralCatchClause
+                catch
+                {
+                }
+            }
+        }
+    }
+}
