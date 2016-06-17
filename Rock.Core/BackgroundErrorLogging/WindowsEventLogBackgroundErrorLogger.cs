8aa44083f582e52de96f66024fe05545e877f22d
Brian Friesen <brianfriesen@quickenloans.com>
2016-06-17 18:14:23 -0400

    Add windows event log logger

---
+++ Rock.Core/BackgroundErrorLogging/WindowsEventLogBackgroundErrorLogger.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+
+namespace Rock.BackgroundErrorLogging
+{
+    public class WindowsEventLogBackgroundErrorLogger : IBackgroundErrorLogger
+    {
+        protected virtual string Source
+        {
+            get { return ".NET Runtime"; }
+        }
+
+        protected virtual string LogName
+        {
+            get { return "Application"; }
+        }
+
+        public void Log(BackgroundErrorLog log)
+        {
+            if (!EventLog.SourceExists(Source))
+            {
+                EventLog.CreateEventSource(Source, LogName);
+            }
+
+            var eventLog = new EventLog("Application")
+            {
+                Source = Source,
+            };
+
+            eventLog.WriteEntry(log.Format(), EventLogEntryType.Error);
+        }
+    }
+}
